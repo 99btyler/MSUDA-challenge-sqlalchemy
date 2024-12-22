462db5848f6b5df26196cc62181874a6c386769b
Tyler Beringer <99tylerberinger@gmail.com>
2024-12-22 13:38:11 -0500

    Finished app setup

--- submission/app.py
+++ submission/app.py
@@ -1,1 +1,25 @@
-# ...
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+
+# database
+engine = create_engine("sqlite:///resources/hawaii.sqlite")
+session = Session(engine)
+
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+
+Station = Base.classes.station
+Measurement = Base.classes.measurement
+
+# flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "this is the index"
+
+# main
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
