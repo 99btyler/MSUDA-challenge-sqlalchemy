ae1d8fd58f10f279ac767b879e4b5c46a8a69ffe
Tyler Beringer <99tylerberinger@gmail.com>
2024-12-22 15:55:41 -0500

    Finished index route

--- submission/app.py
+++ submission/app.py
@@ -18,7 +18,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "this is the index"
+    return  """
+            <html style="font-family:arial; text-align:center;">
+                <head>
+                    <h1>Climate App</h1>
+                </head>
+                <body>
+                    <ul style="list-style-type:none; padding:0;">
+                        <li><span style="color:#006eff;">    /                       </span> - returns the index with available routes</li>
+                        <li><span style="color:#006eff;">    /api/v1.0/precipitation </span> - returns a JSON list of the last 12 months of precipitation data</li>
+                        <li><span style="color:#006eff;">    /api/v1.0/stations      </span> - returns a JSON list of the stations</li>
+                        <li><span style="color:#006eff;">    /api/v1.0/tobs          </span> - returns a JSON list of the last 12 months of temperature data</li>
+                        <li><span style="color:#006eff;">    /api/v1.0/start         </span> - returns a JSON list of the minimum, average, and maximum temperature from the specified range</li>
+                        <li><span style="color:#006eff;">    /api/v1.0/start/end     </span> - returns a JSON list of the minimum, average, and maximum temperature from the specified range</li>
+                    </ul>
+                </body>
+            </htm>
+            """
 
 # main
 if __name__ == "__main__":
