314f1cad1db847110c561317b8a542cc678eabdc
Tyler Beringer <99tylerberinger@gmail.com>
2024-12-22 16:30:18 -0500

    Finished routes: stations and tobs

--- submission/app.py
+++ submission/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -28,13 +28,41 @@ def index():
                         <li><span style="color:#006eff;">    /                       </span> - returns the index with available routes</li>
                         <li><span style="color:#006eff;">    /api/v1.0/precipitation </span> - returns a JSON list of the last 12 months of precipitation data</li>
                         <li><span style="color:#006eff;">    /api/v1.0/stations      </span> - returns a JSON list of the stations</li>
-                        <li><span style="color:#006eff;">    /api/v1.0/tobs          </span> - returns a JSON list of the last 12 months of temperature data</li>
+                        <li><span style="color:#006eff;">    /api/v1.0/tobs          </span> - returns a JSON list of the last 12 months of temperature data for the most-active station</li>
                         <li><span style="color:#006eff;">    /api/v1.0/start         </span> - returns a JSON list of the minimum, average, and maximum temperature from the specified range</li>
                         <li><span style="color:#006eff;">    /api/v1.0/start/end     </span> - returns a JSON list of the minimum, average, and maximum temperature from the specified range</li>
                     </ul>
                 </body>
             </htm>
             """
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    return "returns a JSON list of the last 12 months of precipitation data"
+
+@app.route("/api/v1.0/stations")
+def stations():
+    stations = session.query(Station.station).all()
+    station_list = [station[0] for station in stations]
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    temperature_data = session.query(
+        Measurement.tobs
+    ).filter(
+        Measurement.station == "USC00519281", Measurement.date <= "2017-08-18", Measurement.date >= "2016-08-18"
+    ).all()
+    temperature_data_list = [item[0] for item in temperature_data]
+    return jsonify(temperature_data_list)
+
+@app.route("/api/v1.0/start")
+def start():
+    return "returns a JSON list of the minimum, average, and maximum temperature from the specified range"
+
+@app.route("/api/v1.0/start/end")
+def start_end():
+    return "returns a JSON list of the minimum, average, and maximum temperature from the specified range"
 
 # main
 if __name__ == "__main__":
